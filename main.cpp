b9b27a98a97f3f84e1b688b388630c64d0599795
amestenger <amelia.stenger@student.northampton.edu>
2026-02-02 00:49:46 +0000

    I wrote the script to find the area and perimeter of the rectangle. By referencing the textbook, I was able to write this script no problem!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  const double length = 7.2;
-  const double width = 1.3;
+int main() 
+{ 
+  double length;
+  double width;
+  double area;
+  double perimeter;
 
+  cout << "Rectangle Properties:" << endl;
 
+  length = 7.2;
+  width = 1.3;
+  perimeter = 2 * (length + width) ;
+  area = length * width;
+
+  cout << "Length = " << width << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
+
+  return 0;
 }
